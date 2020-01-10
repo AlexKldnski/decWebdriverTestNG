31b32a7efae93b7734dec2aacd3e5c2efa347726
AlexKldnski <a.p.kolodinsky@gmail.com>
2020-01-10 13:22:16 -0700

    3rd commit

--- src/test/java/CheckBoxesTests.java
+++ src/test/java/CheckBoxesTests.java
@@ -26,14 +26,22 @@ public class CheckBoxesTests {
     }
 
     //TODO: find a different way of checking checkbox state (checked/unchecked)
-    @Test
-    public void testName() {
-    }
+    //DONE isSelected method is applied in AssertCheckbox() method
+
 
     //TODO: automate a different flow
     @Test
     public void test002() {
+        openMainPage();
+        By by = By.xpath("//*[@id=\"content\"]/div/h3");
+        String headertext = "Checkboxes";
+        findHeadermethod(by, headertext);
 
+    }
+
+    private void findHeadermethod(By by, String headertext) {
+        WebElement element = driver.findElement(by);
+        Assert.assertEquals(headertext, element.getText());
     }
 
     //TODO: refactor it
@@ -41,25 +49,39 @@ public class CheckBoxesTests {
     public void test001() {
         openMainPage();
 
-        List<WebElement> listOfInputs = driver.findElements(By.tagName("input"));
+        By by = By.tagName("input");
+        int qtyOfCheckBoxes = 2;
+        boolean[] initialCheckBoxesStateArr = {false, true};
 
-        Assert.assertTrue(listOfInputs.size() == 2, "List of Checkboxes is not equal to " + listOfInputs.size());
+        List<WebElement> listOfInputs = locateCheckBoxes(by);
 
-        WebElement checkBox1 = listOfInputs.get(0);
+        checkQtyOfCheckBoxes(listOfInputs, qtyOfCheckBoxes);
 
-        String isBox1Checked = checkBox1.getAttribute("checked");
-
-        Assert.assertNull(isBox1Checked);
-
-        checkBox1.click();
-
-        Assert.assertEquals(checkBox1.getAttribute("checked"), "true");
-
-        WebElement checkBox2 = listOfInputs.get(1);
-
-        Assert.assertEquals(checkBox2.getAttribute("checked"), "true");
+        for (int i=0; i<listOfInputs.size(); i++){
+            AssertCheckbox(listOfInputs.get(i), initialCheckBoxesStateArr[i]);
+        }
 
     }
+
+    private void checkQtyOfCheckBoxes(List listOfInputs, int qtyOfCheckBoxes) {
+        Assert.assertTrue(listOfInputs.size() == qtyOfCheckBoxes, "List of Checkboxes is not equal to " + qtyOfCheckBoxes);
+    }
+
+    private void AssertCheckbox(WebElement Element, boolean isBoxChecked) {
+        boolean isElementChecked = Element.isSelected();
+        Assert.assertEquals(isBoxChecked, isElementChecked, "Initial Checkbox State is not" + isBoxChecked);
+
+        Element.click();
+
+        Assert.assertEquals(Element.isSelected(), !isElementChecked);
+
+        Element.click();
+    }
+
+    private List locateCheckBoxes(By by){
+        List<WebElement> listOfInputs = driver.findElements(by);
+        return listOfInputs;
+    };
 
     private void openMainPage() {
         driver.get("https://the-internet.herokuapp.com/checkboxes");
