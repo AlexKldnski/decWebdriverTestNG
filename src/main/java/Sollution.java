c04d0bcf4c59f42f8e0e20a381cedb59ada0a699
AlexKldnski <a.p.kolodinsky@gmail.com>
2021-01-14 13:02:04 -0700

    initial commit

---
+++ src/main/java/Sollution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Sollution {
+    public static void main(String[] args) {
+//        Scanner sc=new Scanner(System.in);
+//        System.out.println("================================");
+//        for(int i=0;i<3;i++){
+//            String s1=sc.next();
+//            int x=sc.nextInt();
+//            System.out.printf("%-15s%03d%n", s1, x);
+//        }
+//        System.out.println("================================");
+
+//        Scanner in = new Scanner(System.in);
+//        int t=in.nextInt();
+//        for(int i=0;i<t;i++){
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            int n = in.nextInt();
+//            int x=a;
+//            for (int j=0; j<=n-1; j++) {
+//                x = x + (int)((Math.pow(2,j))*b);
+//                System.out.print (x + " ");
+//            }
+//            System.out.println();
+//        }
+//        in.close();
+
+
+    }
+}
